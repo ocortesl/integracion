9f5d517bda72c5ba66549ffe843b82f7975795f1
Osbaldo <oscolo79@gmail.com>
2021-12-17 18:17:57 -0500

    commit 7

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <h1>Integración avance 1</h1>
+          <h1>Integración avance 2</h1>
         </p>
         <a
           className="App-link"
